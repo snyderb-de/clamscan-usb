9e7d1a11ea6b492256c2c83593993f5868ec5b7c
Bryan Snyder <snyderb.de@gmail.com>
2023-03-19 22:00:19 -0400

    swapped tkinter and osascript in for zenity

--- usb_scan.sh
+++ usb_scan.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Prompt the user to select a USB drive
-USB_DRIVE=$(zenity --file-selection --directory --title="Select a USB drive to scan")
-
-# Check if the user canceled the selection
-if [ -z "$USB_DRIVE" ]; then
-    zenity --error --text="No USB drive selected."
-    exit 1
+# Check the operating system and set the folder and output variables accordingly
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS: Use AppleScript to get the folder and output paths
+  folder=$(osascript -e 'try' -e 'set _folder to choose folder with prompt "Select folder to scan"' -e 'POSIX path of _folder' -e 'on error' -e '' -e 'end try')
+  output=$(osascript -e 'try' -e 'set _folder to choose folder with prompt "Select where to save log"' -e 'POSIX path of _folder' -e 'on error' -e '' -e 'end try')
+else
+  # Other platforms: Use Python's tkinter to get the folder and output paths
+  folder=$(python3 -c 'import tkinter as tk; from tkinter import filedialog; root = tk.Tk(); root.withdraw(); folder = filedialog.askdirectory(title="Select folder to scan"); print(folder)' 2>/dev/null)
+  output=$(python3 -c 'import tkinter as tk; from tkinter import filedialog; root = tk.Tk(); root.withdraw(); folder = filedialog.askdirectory(title="Select where to save log"); print(folder)' 2>/dev/null)
 fi
 
-# Run the virus scan and save the results to clamscan.log
-sudo -u $USER stdbuf -oL clamscan -r -i -v "$USB_DRIVE" 2>&1 | tee "$HOME/clamscan.log" | zenity --text-info --title="Scan Results" --width=800 --height=600 --auto-scroll
-
-# Show the scan results to the user
-zenity --text-info --title="Scan Results" --width=800 --height=600 --filename="$HOME/clamscan.log"
-
+# Run the virus scan and save the results to clamscan.log in the selected output directory
+sudo -u "$USER" stdbuf -oL clamscan -r -i -v "$folder" 2>&1 | tee "${output}/clamscan.log"
